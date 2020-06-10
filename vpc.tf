a34086c3480afc92838a4355ea8143d2aacf6309
jimbu-1 <66702998+jimbu-1@users.noreply.github.com>
2020-06-10 16:39:55 -0500

    module

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,8 @@
 module "vpc" {
   source  = "app.terraform.io/SFTerr/vpc/aws"
   version = "2.39.0"
-  resource aws_vpc "hashicatvj" {
   cidr_block           = var.address_space
-  enable_dns_hostnames = true
-
   tags = {
     name = "${var.prefix}-vpc"
   }
 }
-}
-  
